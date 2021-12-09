63b7ef97db1fdd4ea311b2e2fe791494dc4a6b36
Alexey Levchuk <bizlex@yandex.ru>
2021-12-09 19:21:40 +0300

    Install validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,11 @@
-const getNotes = require('./utils')
+const validator = require('validator')
+const getNotes = require('./notes.js')
 
 console.log(getNotes)
 
+console.log(validator.isEmail('alex@test.com'));
+console.log(validator.isURL('http//'));
+
 // const add = require('./utils')
-
 // const sum = add(5, 3)
-
 // console.log(sum);
