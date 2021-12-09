f1361fb61276204117bbe6f4b97880349aa52963
Alexey Levchuk <bizlex@yandex.ru>
2021-12-09 20:05:23 +0300

    Add chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,18 @@
-const validator = require('validator')
+// const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 console.log(getNotes)
+console.log(chalk.green.bold.inverse('Success!'))
+const log = console.log;
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
 
-console.log(validator.isEmail('alex@test.com'));
-console.log(validator.isURL('http//'));
+// console.log(validator.isEmail('alex@test.com'));
+// console.log(validator.isURL('http//'));
 
 // const add = require('./utils')
 // const sum = add(5, 3)
