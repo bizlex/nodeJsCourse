3b490162edf2cb4205bd9ae2894d5b93c7692e68
Alexey Levchuk <bizlex@yandex.ru>
2021-12-10 13:38:36 +0300

    Study process.agrv()

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,11 @@
-// const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-console.log(getNotes)
-console.log(chalk.green.bold.inverse('Success!'))
-const log = console.log;
-log(chalk.green(
-	'I am a green line ' +
-	chalk.blue.underline.bold('with a blue substring') +
-	' that becomes green again!'
-));
+const command = process.argv[2]
+console.log(process.argv);
 
-// console.log(validator.isEmail('alex@test.com'));
-// console.log(validator.isURL('http//'));
-
-// const add = require('./utils')
-// const sum = add(5, 3)
-// console.log(sum);
+if (command === 'add') {
+	console.log('Adding note!');
+} else if (command === 'remove') {
+	console.log('Removing note!');
+}
