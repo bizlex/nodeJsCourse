821bb957815064c41f2fe5dfd145e7365e4a86e9
Alexey Levchuk <bizlex@yandex.ru>
2021-12-11 16:27:03 +0300

    Add yargs package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,53 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
-console.log(process.argv);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Body: ', argv.body);
+        console.log('Title: ', argv.title);
+    }
+})
 
-if (command === 'add') {
-	console.log('Adding note!');
-} else if (command === 'remove') {
-	console.log('Removing note!');
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('Listing the notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note');
+    }
+})
+
+yargs.parse()
+// console.log(yargs.argv);
