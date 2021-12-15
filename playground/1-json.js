aa51a77189d8b6f13c143c2663bbb73b8c37e97f
Alexey Levchuk <bizlex@yandex.ru>
2021-12-15 21:15:13 +0300

    Fix 1-json.js file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,16 +1,4 @@
 const fs = require('fs')
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday'
-// }
-
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json', bookJSON)
-
-// const dataBuffer = fs.readFileSync('1-json.json')
-// const dataJSON = dataBuffer.toString()
-// const data = JSON.parse(dataJSON)
-// console.log(data.title);
 
 const data = JSON.parse(fs.readFileSync('1-json.json').toString())
 data.name = 'Alex'
