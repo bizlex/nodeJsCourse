16f92cb257beded4810cd9e11f7b7d66ee28287a
Alexey Levchuk <bizlex@yandex.ru>
2021-12-22 12:35:37 +0300

    Add read command

--- notes-app/app.js
+++ notes-app/app.js
@@ -51,8 +51,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler() {
-        console.log('Reading the note');
+    builder: {
+        title: {
+            describe: 'Title note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,9 +4,9 @@ const getNotes = () => 'Your notes...'
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -34,9 +34,20 @@ const removeNote = (title) => {
 
 const listNotes = () => {
     const notes = loadNotes()
-
     console.log(chalk.inverse('Your notes...'));
     notes.forEach(note => console.log(note.title))
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find(note => note.title === title)
+    
+    if (note) {
+        console.log(chalk.inverse(note.title));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red.inverse('Note not found!'));
+    }
 }
 
 const saveNotes = (notes) => {
@@ -58,5 +69,6 @@ module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
